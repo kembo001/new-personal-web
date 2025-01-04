ec1aefcfe160a02646fac1383f3f794305033fa3
Brandon <brandonkemboi87@gmail.com>
2025-01-03 18:44:05 -0600

    Installed React and Edited CSS

---
+++ personal-web/src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Header from './componenets/Header';
+import Banner from './componenets/Banner';
+
+
+function App() {
+  return (
+    <div className="App smooth-scroll">
+      <Header />
+      <section>
+      <Banner />
+        </section>
+        <section>
+
+        </section>
+        <section>
+        
+        </section>
+        <section>
+        
+        </section>
+    </div>
+  );
+}
+
+export default App;
---
+++ personal-web/src/componenets/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+      <div className='logo'>
+        BK
+      </div>
+
+      <div className='contact'>
+        Contact Me
+      </div>
+    </div>
+  )
+}
+
+export default Header
